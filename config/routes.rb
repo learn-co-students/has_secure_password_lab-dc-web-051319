d604a5a7fa8300d42896c57bd5eb39bc708e78d8
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-16 15:31:22 -0400

    Completed lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :users
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
