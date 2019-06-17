0c692aba070c70ff6c9a68ca7bdfa881ebfd8481
Your Name <you@example.com>
2019-06-17 06:33:05 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  resources :users, only: [:new, :create]
+
+
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    post '/logout' => 'sessions#destroy'
+
+    root 'welcome#home'
+
+  end
